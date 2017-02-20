b7b5e8cf68fda6428f3528d490e4562e258238d1
Logan Smith <hamspamandeggs@gmail.com>
2017-02-20 15:47:47 +0000

    initial api setup with seed data etc

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+Show.delete_all()
+
+Show.create({
+  title: "Family Guy",
+  series: 10,
+  description: "Adult Cartoon Comedy",
+  image: "http://is1.mzstatic.com/image/thumb/Video6/v4/6c/1b/ed/6c1bed53-61ef-4a2e-2413-08078efacf75/mzl.ybhuiewd.lsr/600x600bb-85.jpg",
+  programmeID: "fgtv010"
+  })
+
+Show.create({
+  title: "Garth Marenghi's Dark Place",
+  series: 1,
+  description: "Alternative Comedy",
+  image: "http://i.onionstatic.com/avclub/4410/02/16x9/960.jpg",
+  programmeID: "gmdptv001"
+  })
